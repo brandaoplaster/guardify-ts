c5a766ce1241f4d7173ca99e159ef05bb9104b66
brandaoplaster <brandaoplaster@gmail.com>
2024-10-25 20:59:47 -0300

    feat: Adding routes

--- src/router/index.ts
+++ src/router/index.ts
@@ -1,11 +1,37 @@
 import { createRouter, createWebHistory } from "vue-router"
 import HomePage from "../pages/HomePage.vue";
+import AuthTemplate from "../layouts/AuthTemplate.vue";
+import LoginPage from "../pages/auth/LoginPage.vue";
+import ForgotPassword from "../pages/auth/ForgotPassword.vue";
+import DefaultTemplate from "../layouts/DefaultTemplate.vue";
 
 const routes = [
   {
-    name: "home",
-    component: HomePage,
-    path: "/"
+    path: "/",
+    component: AuthTemplate,
+    children: [
+      {
+        path: "",
+        component: LoginPage,
+        name: "auth.login"
+      },
+      {
+        path: "forgot-password",
+        component: ForgotPassword,
+        name: "forgot.password"
+      }
+    ]
+  },
+  {
+    path: "/admin",
+    component: DefaultTemplate,
+    children: [
+      {
+        path: "",
+        component: HomePage,
+        name: "admin.home"
+      }
+    ]
   }
 ]
 

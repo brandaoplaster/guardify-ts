fab3e11017b6b91aa224619b05b74bbfed7e6354
brandaoplaster <brandaoplaster@gmail.com>
2024-10-26 20:35:32 -0300

    feat: Mapping routers

---
+++ src/router/routerd.map.ts
@@ -0,0 +1,35 @@
+import HomePage from "../pages/HomePage.vue";
+import AuthTemplate from "../layouts/AuthTemplate.vue";
+import LoginPage from "../pages/auth/LoginPage.vue";
+import ForgotPassword from "../pages/auth/ForgotPassword.vue";
+import DefaultTemplate from "../layouts/DefaultTemplate.vue";
+
+export default [
+  {
+    path: "/",
+    component: AuthTemplate,
+    children: [
+      {
+        path: "",
+        component: LoginPage,
+        name: "auth.login"
+      },
+      {
+        path: "forgot-password",
+        component: ForgotPassword,
+        name: "forgot.password"
+      }
+    ]
+  },
+  {
+    path: "/admin",
+    component: DefaultTemplate,
+    children: [
+      {
+        path: "",
+        component: HomePage,
+        name: "admin.home"
+      }
+    ]
+  }
+]

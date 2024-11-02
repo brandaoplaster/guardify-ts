23c4816dbb785f2da337fb596c802de0e7e6b976
brandaoplaster <brandaoplaster@gmail.com>
2024-11-01 21:18:42 -0300

    chore: Adding token in localstorage

--- src/infra/services/UserGateway.ts
+++ src/infra/services/UserGateway.ts
@@ -1,11 +1,12 @@
-import { User } from "../../types/User";
+import { TOKEN } from "../../helpers/constants";
 import httpAdapter from "../http/HttpClientAdapter";
 
 export default class UserService {
-  async login(email: string, password: string): Promise<User> {
+  async login(email: string, password: string): Promise<any> {
     return await httpAdapter.post("/login", {
       email,
       password,
-    });
+    })
+    .then(response => localStorage.setItem(TOKEN, response.data.token))
   }
 }
